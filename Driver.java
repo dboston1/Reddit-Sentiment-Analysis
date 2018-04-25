dee463ed81bbf5de08c4f849ef67d9162a8faf74
Drew Boston <31427655+dboston1@users.noreply.github.com>
2018-04-24 23:18:15 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -16,6 +16,25 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
+// Hadoop Counters
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
+
+// Hadoop File System
+import org.apache.hadoop.fs.FileSystem;
+
+// Java file IO 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.StringTokenizer;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
 //to Compile: $HADOOP_HOME/bin/hadoop com.sun.tools.javac.Main *.java -d build -Xlint (make a "build" directory first
 // then: jar -cvf SentimentAnalysis.jar -C build/ .
 // to run: $HADOOP_HOME/bin/hadoop jar SentimentAnalysis.jar org.SentimentAnalysis.Driver /termProject/test.txt /test-output -negation /termProject/negate-words.txt -pos /termProject/pos-words.txt -neg /termProject/neg-words.txt
@@ -57,7 +76,34 @@ public class Driver extends Configured implements Tool {
 		job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     int result = job.waitForCompletion(true) ? 0 : 1;
-	 
+	
+	// parse counters and output to file
+	Counters counters = job.getCounters();
+	float hillary = counters.findCounter("org.SentimentAnalysis.Map$CTR", "HILLARY").getValue();
+	//System.out.printf("\n%d\n", counters.findCounter("org.SentimentAnalysis.Map$CTR", "HILLARY").getValue());
+	float trump = counters.findCounter("org.SentimentAnalysis.Map$CTR", "TRUMP").getValue();
+	float both = counters.findCounter("org.SentimentAnalysis.Map$CTR", "BOTH").getValue();
+	
+	System.out.printf("\nH: %f T: %f B: %f\n", hillary, trump, both);
+	
+	FileSystem hdfs = FileSystem.get( new URI("hdfs://montgomery:40181"), getConf());
+	Path file = new Path("hdfs://montgomery:40181/count.txt");
+	if(hdfs.exists(file)){
+		hdfs.delete(file, true);
+	}
+	OutputStream os = hdfs.create(file);
+	BufferedWriter br = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+	br.write("HILLARY: " + hillary + "\n");
+	br.write("TRUMP: " + trump + "\n");
+	br.write("BOTH: " + both + "\n");
+	float total = hillary + trump + both;
+	br.write("TOTAL COMMENTS: " + total + "\n");
+	br.close();
+	hdfs.close();
+	
+	
+	
     return result;
   }
 }
