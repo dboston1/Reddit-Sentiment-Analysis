acffb14f23b0b1d99c64c9749d2a0dff76245abb
Drew Boston <31427655+dboston1@users.noreply.github.com>
2018-04-24 23:19:56 -0600

    Update Reduce.java

--- Reduce.java
+++ Reduce.java
@@ -17,20 +17,30 @@ import org.apache.hadoop.util.StringUtils;
 
 public class Reduce extends 
 	Reducer<Text, IntWritable, Text, IntWritable> {
-@Override
+	private final static IntWritable plusOne = new IntWritable(1);
+	private final static IntWritable minusOne = new IntWritable(-1);
+	private final static IntWritable zero = new IntWritable(0);
+	@Override
 	public void reduce(Text word, Iterable<IntWritable> instances, Context context)
 			throws IOException, InterruptedException
 	{
-		
 		int sum = 0;
 
 		// Sum up the instances (sentiment values) of the current word. Note that unless docId's match, there should only be one value in instances.
 		for (IntWritable instance : instances) {
-			//sum += instance.get();
-			context.write(word, instance);
+			sum += instance.get();
+			//context.write(word, instance);
 		}
 		
 		// Write the word and count to output.
-		//context.write(word, new IntWritable(sum));
+		if(sum > 0){
+			context.write(word, plusOne);
+		}
+		else if(sum == 0){
+			context.write(word, zero);
+		}
+		else{
+			context.write(word, minusOne);
+		}
 	}
 }
